373025fc605a49ee0c899b7d27c4ae64268c8ba9
zachmshort <zach.short@fantomworks.com>
2024-12-19 12:42:43 -0500

    unused vars

--- components/room-form.tsx
+++ components/room-form.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { ColorSelect } from "@/components/color-select-drawer";
-import { sulpherBold, sulpherLight } from "@/components/fonts";
+import { sulpherBold } from "@/components/fonts";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 import Logo from "./logo";
