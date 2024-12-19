f4e886cdcdd9f7fde6d9f51023a5ff33e013e8e8
zachmshort <zach.short@fantomworks.com>
2024-12-19 18:01:39 -0500

    unused vars

--- app/monopoly/room/[code]/page.tsx
+++ app/monopoly/room/[code]/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useRef } from "react";
+// import { useEffect, useRef } from "react";
 import { use } from "react";
 import RoomView from "../components/room-view";
 
