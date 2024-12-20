97f955bc52aad746d571c4ab6bd79e7c097bf16b
zachmshort <zach.short@fantomworks.com>
2024-12-20 13:04:52 -0500

    null error

--- components/players/card-content.tsx
+++ components/players/card-content.tsx
@@ -8,11 +8,11 @@ const PlayerDetails = ({ player }: { player: Player }) => {
         className={`text-black flex flex-col items-evenly gap-y-2 justify-between ${josephinBold.className} text-2xl`}
       >
         <div className={`flex items-center justify-center mt-4`}>
-          <div>${player?.balance}</div>
+          <div>${player?.balance || 0}</div>
         </div>
         <div className={`flex items-center justify-between`}>
           <div>Properties</div>
-          <div>{player.properties.length}</div>
+          <div>{player?.properties?.length || 0}</div>
         </div>
         <div className={`flex items-center justify-between`}>
           <div>Monopolies</div>
