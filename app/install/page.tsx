c16b92f7d111b3c1b6d5d81af45de0f738614161
zachmshort <zach.short@fantomworks.com>
2024-12-21 12:40:54 -0500

    button styling

--- app/install/page.tsx
+++ app/install/page.tsx
@@ -78,7 +78,12 @@ function InstallPrompt() {
           Install
         </button>
       )}
-
+      <Link
+        href="/"
+        className={`border font rounded-lg p-4 border-yellow-200 w-64  text-black text-2xl`}
+      >
+        Home
+      </Link>
       {isIOS && (
         <p>
           To install this app on your iOS device, tap the share button
